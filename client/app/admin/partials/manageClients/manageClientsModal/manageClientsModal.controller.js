c0df11d72d944f569ffbe73fe35c6801488d1e68
nicolasiscoding <nicolascoding@gmail.com>
2016-03-12 17:47:14 -0500

    can edit a existing client

--- client/app/admin/partials/manageClients/manageClientsModal/manageClientsModal.controller.js
+++ client/app/admin/partials/manageClients/manageClientsModal/manageClientsModal.controller.js
@@ -1,14 +1,37 @@
 'use strict';
 
 angular.module('essenceEventsRepoApp.admin')
-  .controller('ManageClientsModalCtrl', function ($scope, $modalInstance, user)
+  .controller('ManageClientsModalCtrl', ['$scope', '$modalInstance', 'user', function ($scope, $modalInstance, user)
   {
     $scope.username = user.name;
-    $scope.phoneNumber = user.phoneNumber;
     $scope.email = user.email;
-    $scope.submit = function(){
-      console.log('called save function');
+    $scope.phoneNumber = user.phoneNumber;
+
+    $scope.submit = function(username, email, phoneNumber) {
+        console.log(username);
+        //console.log();
+        // user.name = "Hello!";
+        // user.email = "2@3.com";
+        // user.phoneNumber = "87354";
+        console.log(email);
+        console.log(phoneNumber);
+        // // console.log(user);
+        // // user.$save();
+        // /*var user = {
+        //   username: $scope.username,
+        //   email: $scope.email,
+        //   phoneNumber: $scope.phoneNumber
+        // };
+        // *
+        //user.$save();
+        user.name = username;
+        user.email = email;
+        user.phoneNumber = phoneNumber;
+        user.$save();
+        console.log(user);
+
+        $modalInstance.close();
     };
 
-    // console.log(user);
-  });
+
+  }]);
--- server/api/user/index.js
+++ server/api/user/index.js
@@ -11,6 +11,7 @@ router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/:id', auth.isAuthenticated(), controller.updateUser);
 router.post('/', auth.hasRole('admin'), controller.create);
 
 export default router;
