2a07d6b527cd0519553044deef54aae7ca4f2b3b
hanif <hanif314@gmail.com>
2016-03-12 13:20:09 -0500

    working on submit button

--- client/app/admin/partials/manageClients/manageClientsModal/manageClientsModal.controller.js
+++ client/app/admin/partials/manageClients/manageClientsModal/manageClientsModal.controller.js
@@ -1,12 +1,33 @@
 'use strict';
 
-angular.module('essenceEventsRepoApp.admin')
-  .controller('ManageClientsModalCtrl', function ($scope, $modalInstance, user)
-  {
+class ManageClientsModalCtrl{
+  constructor($scope, $modalInstance, user){
     $scope.username = user.name;
     $scope.phoneNumber = user.phoneNumber;
     $scope.email = user.email;
 
+  }
 
-    console.log(user);
-  });
+  submit(form){
+    console.log('we here motherfucker');
+  }
+
+
+};
+
+angular.module('essenceEventsRepoApp.admin')
+  .controller('ManageClientsModalCtrl', ManageClientsModalCtrl);
+
+// angular.module('essenceEventsRepoApp.admin')
+//   .controller('ManageClientsModalCtrl', function ($scope, $modalInstance, user)
+//   {
+//     $scope.username = user.name;
+//     $scope.phoneNumber = user.phoneNumber;
+//     $scope.email = user.email;
+//     $scope.save = function(){
+//
+//       console.log('called save function');
+//     };
+//
+//     console.log(user);
+//   });
