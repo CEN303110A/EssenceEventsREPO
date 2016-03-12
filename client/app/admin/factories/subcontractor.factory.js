a882b716c364a895a856b1e4785144505bc06839
WilliamWickerson <willie7296@gmail.com>
2016-03-12 16:10:36 -0500

    subcontractor

---
+++ client/app/admin/factories/subcontractor.factory.js
@@ -0,0 +1,14 @@
+angular.module('essenceEventsRepoApp.admin').factory('Subcontractors', ['$http',
+  function($http) {
+    var methods = {
+      getAll: function() {
+        return $http.get('http://localhost:8080/api/subcontractors');
+      },
+
+      create: function(contractor) {
+	return $http.post('http://localhost:8080/api/subcontractors', contractor);
+      }
+    };
+    return methods;
+  }
+]);
---
+++ server/api/subcontractor/subcontractor.controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import Subcontractor from './subcontractor.model';
+import passport from 'passport';
+import config from '../../config/environment';
+import jwt from 'jsonwebtoken';
+
+export function index(req, res) {
+  Subcontractor.findAsync({}, 'name')
+    .then(subcontractors => {
+      res.status(200).json(subcontractors);
+    })
+    .catch(handleError(res));
+}
+
+export function create(req, res) {
+  console.log('reached');
+  var newSubcontractor = new Subcontractor(req.body);
+  newSubcontractor.saveAsync()
+    .then(res.send())
+    .catch(validationError(res));
+}
