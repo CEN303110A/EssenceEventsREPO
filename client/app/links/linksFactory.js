9e6fdba4c5e4b41158b66f9df7f4eaefca013133
Chase Farmer <chasefarmer2808@ufl.edu>
2016-03-16 17:47:35 -0400

    functional links view

--- client/app/links/linksFactory.js
+++ client/app/links/linksFactory.js
@@ -6,25 +6,28 @@ angular.module('essenceEventsRepoApp').factory('Links', function() {
         "name": "Allgood Photography",
         "site": "http://www.allgood-photography.com",
         "phoneNumber": "(904)945-8879",
-        "img": "Hello"
+        "img": "assets/images/links/allgoodphotography.jpg"
       },
       {
         "type": "Photography",
         "name": "Photogrpahy by Ivy Hammer",
         "site": "http://www.ivyhammerphoto.com",
-        "phoneNumber": "(352)870-3808"
+        "phoneNumber": "(352)870-3808",
+        "img": ""
       },
       {
         "type": "Rental Chair Covers and Sashes",
         "name": "Fancy That! of Ganesville",
         "site": "http://www.fancythatonline.com",
-        "phoneNumber": "(352)505-6929"
+        "phoneNumber": "(352)505-6929",
+        "img": ""
       },
       {
         "type": "Invitations",
         "name": "Forever Graphics & Printing",
         "site": "http://www.forevergraphics.com",
-        "phoneNumber": "(352)246-4620"
+        "phoneNumber": "(352)246-4620",
+        "img": "assets/images/links/fgp1.png"
       }
     ]
   };
