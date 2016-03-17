8e9eb7e53fdd400ac66f9c61f65db2ecb25cf9e2
WilliamWickerson <willie7296@gmail.com>
2016-03-17 18:10:06 -0400

    more minor changes

--- client/app/admin/partials/manageEvent/manageEvent.controller.js
+++ client/app/admin/partials/manageEvent/manageEvent.controller.js
@@ -9,20 +9,5 @@ angular.module('essenceEventsRepoApp.admin')
 	}, function(err) {
 	  //do something
       });
-    }
-    $scope.remove = function(id) {
-      var event = {
-	_id: id,
-	userId: "56e9d5f06874ee9e0770faca",
-	name: "change",
-        description: "A party of sorts",
-	date: "today sometime " + new Date()
-      };
-      Events.update(event)
-	.then(function(response) {
-	  $state.reload();
-	}, function(err) {
-	  //do something
-      });
-    }
+    };
 }]);
