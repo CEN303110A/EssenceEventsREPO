6adfdbb73af0725d083ebe5b2adf997bc1c45fe8
nicolasiscoding <nicolascoding@gmail.com>
2016-03-18 16:23:14 -0400

    changed hardcoded ports to 9000, Willie will work on dynamically

--- client/app/admin/factories/event.factory.js
+++ client/app/admin/factories/event.factory.js
@@ -2,27 +2,27 @@ angular.module('essenceEventsRepoApp.admin').factory('Events', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/events');
+        return $http.get('http://localhost:9000/api/events');
       },
 
       getByUser: function(userId) {
-	return $http.get('http://localhost:8080/api/events/user/' + userId);
+	return $http.get('http://localhost:9000/api/events/user/' + userId);
       },
 
       getOne: function(id) {
-	return $http.get('http://localhost:8080/api/events/' + id);
+	return $http.get('http://localhost:9000/api/events/' + id);
       },
 
       create: function(event) {
-	return $http.post('http://localhost:8080/api/events', event);
+	return $http.post('http://localhost:9000/api/events', event);
       },
 
       update: function(event) {
-	return $http.put('http://localhost:8080/api/events', event);
+	return $http.put('http://localhost:9000/api/events', event);
       },
 
       remove: function(id) {
-	return $http.delete('http://localhost:8080/api/events/' + id);
+	return $http.delete('http://localhost:9000/api/events/' + id);
       }
     };
     return methods;
--- client/app/admin/factories/subcontractor.factory.js
+++ client/app/admin/factories/subcontractor.factory.js
@@ -2,23 +2,23 @@ angular.module('essenceEventsRepoApp.admin').factory('Subcontractors', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/subcontractors');
+        return $http.get('http://localhost:9000/api/subcontractors');
       },
 
       getOne: function(id) {
-	return $http.get('http://localhost:8080/api/subcontractors/' + id);
+	return $http.get('http://localhost:9000/api/subcontractors/' + id);
       },
 
       create: function(subcontractor) {
-	return $http.post('http://localhost:8080/api/subcontractors', subcontractor);
+	return $http.post('http://localhost:9000/api/subcontractors', subcontractor);
       },
 
       update: function(subcontractor) {
-	return $http.put('http://localhost:8080/api/subcontractors', subcontractor);
+	return $http.put('http://localhost:9000/api/subcontractors', subcontractor);
       },
 
       remove: function(id) {
-	return $http.delete('http://localhost:8080/api/subcontractors/' + id);
+	return $http.delete('http://localhost:9000/api/subcontractors/' + id);
       }
     };
     return methods;
