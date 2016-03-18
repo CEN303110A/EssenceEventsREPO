970e3bc5374a8d8675015a163192b394894894a6
hanif <hanif314@gmail.com>
2016-03-18 18:00:25 -0400

    blah

--- client/app/customer/customer.controller.js
+++ client/app/customer/customer.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('essenceEventsRepoApp')
-  .controller('CustomerCtrl', [  '$scope', '$stateParams', '$state', function (  $scope, $stateParams, $state) {
+  .controller('CustomerCtrl', [ 'Events' '$scope', '$stateParams', '$state', function ( Events,  $scope, $stateParams, $state) {
     $scope.message = 'Hello';
     $scope.clientName = $stateParams.userName;
     $scope.id = $stateParams.userID;
