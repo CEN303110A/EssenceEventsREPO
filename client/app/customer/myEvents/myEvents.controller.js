d3ba4be2eee99b8f3179a31da284138e754341b8
Taariq Imami <timami@ufl.edu>
2016-03-19 18:01:54 -0400

    Working pi chart on clientside

--- client/app/customer/myEvents/myEvents.controller.js
+++ client/app/customer/myEvents/myEvents.controller.js
@@ -12,8 +12,29 @@ angular.module('essenceEventsRepoApp')
     $scope.ev_name = the_event.name;
     $scope.ev_date = the_event.date;
     $scope.ev_loc  = the_event.location;
+    $scope.ev_budget = the_event.budget;
     console.log(the_event);
-
-  };
-
+    console.log($scope.ev_budget);
+  }
+    // Pi chart for budget
+    $scope.options = {
+      chart: {
+        type: 'pieChart',
+        height: 500,
+        x: function(d){return d.title;},
+        y: function(d){return d.amount;},
+        showLabels: true,
+        duration: 500,
+        labelThreshold: 0.01,
+        labelSunbeamLayout: true,
+        legend: {
+          margin: {
+            top: 5,
+            right: 35,
+            bottom: 5,
+            left: 0
+          }
+        }
+      }
+    };
 }]);
