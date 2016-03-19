91b08e0f3d2b7497d77e6df5c8a067a8030933d1
nicolasiscoding <nicolascoding@gmail.com>
2016-03-19 17:37:12 -0400

    made calendars independent in the manageEventModal, still looking to fix the multiple click bug

--- client/app/admin/partials/manageEvent/manageEventModal/manageEventModal.controller.js
+++ client/app/admin/partials/manageEvent/manageEventModal/manageEventModal.controller.js
@@ -1,103 +1,116 @@
 'use strict';
 
 angular.module('essenceEventsRepoApp.admin')
-  .controller('ManageEventModalCtrl', ['$scope', '$state', '$q', '$modalInstance', 'event', 'Events', 'Subcontractors', function ($scope, $state, $q, $modalInstance, event, Events, Subcontractors)
+.controller('ManageEventModalCtrl', ['$scope', '$state', '$q', '$modalInstance', 'event', 'Events', 'Subcontractors', function ($scope, $state, $q, $modalInstance, event, Events, Subcontractors)
+{
+  $scope.event = JSON.parse(JSON.stringify(event));
+  $scope.event.subcons = [];
+  $scope.currentCost = 0;
+  for (var i = 0; i < $scope.event.budget.length; i++)
+  $scope.currentCost = $scope.currentCost + $scope.event.budget[i].amount;
+
+
+  //this is for Basic Info
+  $scope.open = function($event) {
+    console.log($event);
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.opened = true;
+  };
+
+
+  //used for Schedule Tab,
+  $scope.newDueDate = new Date();
+  $scope.scheduleDateOpen = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.scheduleDateopened = true;
+  };
+
+  //adds item into todo list
+  $scope.addToDo = function(todo, dueDate)
   {
-      $scope.event = JSON.parse(JSON.stringify(event));
-      $scope.event.subcons = [];
-      $scope.currentCost = 0;
-      for (var i = 0; i < $scope.event.budget.length; i++)
-	$scope.currentCost = $scope.currentCost + $scope.event.budget[i].amount;
+    if (todo && dueDate)
+    $scope.event.toDoList.push({todo: todo, by: dueDate});
+  };
 
-      $scope.open = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.opened = true;
-      };
 
-      $scope.getContractors = function() {
-	Subcontractors.getAll()
-	  .then(function(response) {
-	    $scope.subcontractors = response.data;
-	  }, function(err) {
-	    //do something
-	});
-      };
+  $scope.getContractors = function() {
+    Subcontractors.getAll()
+    .then(function(response) {
+      $scope.subcontractors = response.data;
+    }, function(err) {
+      //do something
+    });
+  };
 
-      $scope.getEventSubcons = function() {
-	var promises = $scope.event.subcontractors.map(function(subcon) {
-	  return Subcontractors.getOne(subcon);
-	});
-	$q.all(promises)
-	  .then(function(response) {
-	    response.forEach(function(r) {
-	      $scope.event.subcons.push(r.data);
-	    });
-	  }, function(err) {
-	    //do something
-	});
-      };
+  $scope.getEventSubcons = function() {
+    var promises = $scope.event.subcontractors.map(function(subcon) {
+      return Subcontractors.getOne(subcon);
+    });
+    $q.all(promises)
+    .then(function(response) {
+      response.forEach(function(r) {
+        $scope.event.subcons.push(r.data);
+      });
+    }, function(err) {
+      //do something
+    });
+  };
 
-      $scope.addSubcontractor = function(contractor) {
-	if ($scope.event.subcontractors.indexOf(contractor._id) === -1) {
-	  $scope.event.subcons.push(contractor);
-	  $scope.event.subcontractors.push(contractor._id);
-	}
-      };
+  $scope.addSubcontractor = function(contractor) {
+    if ($scope.event.subcontractors.indexOf(contractor._id) === -1) {
+      $scope.event.subcons.push(contractor);
+      $scope.event.subcontractors.push(contractor._id);
+    }
+  };
 
-      $scope.addGuest = function(name, email, phoneNumber, partySize) {
-	if(name && email)
-	  $scope.event.guests.push({name: name, email: email, phoneNumber: phoneNumber, partySize: partySize, accepted: false});
-      };
+  $scope.addGuest = function(name, email, phoneNumber, partySize) {
+    if(name && email)
+    $scope.event.guests.push({name: name, email: email, phoneNumber: phoneNumber, partySize: partySize, accepted: false});
+  };
 
-      $scope.deleteGuest = function(index) {
-	$scope.event.guests.splice(index, 1);
-      };
+  $scope.deleteGuest = function(index) {
+    $scope.event.guests.splice(index, 1);
+  };
 
-      $scope.hasItems = function(arr)
-      {
-        return (arr.length > 0);
-      };
+  $scope.hasItems = function(arr)
+  {
+    return (arr.length > 0);
+  };
 
-      //adds item into todo list
-      $scope.addToDo = function(todo, dueDate)
-      {
-	if (todo && dueDate)
-          $scope.event.toDoList.push({todo: todo, by: dueDate});
-      };
+  $scope.addBudget = function(item, cost)
+  {
+    if (item && cost) {
+      $scope.event.budget.push({title: item, amount: cost});
+      $scope.currentCost = $scope.currentCost + cost;
+    }
+  }
 
-      $scope.addBudget = function(item, cost)
-      {
-	if (item && cost) {
-	  $scope.event.budget.push({title: item, amount: cost});
-	  $scope.currentCost = $scope.currentCost + cost;
-        }
-      }
+  //deletes item from todo list
+  $scope.deleteToDo = function(index) {
+    $scope.event.toDoList.splice(index, 1);
+  };
 
-      //deletes item from todo list
-      $scope.deleteToDo = function(index) {
-	$scope.event.toDoList.splice(index, 1);
-      };
+  $scope.deleteBudget = function(index)
+  {
+    $scope.currentCost = $scope.currentCost - $scope.event.budget[index].amount;
+    $scope.event.budget.splice(index, 1);
+  };
 
-      $scope.deleteBudget = function(index)
-      {
-	$scope.currentCost = $scope.currentCost - $scope.event.budget[index].amount;
-        $scope.event.budget.splice(index, 1);
-      };
+  $scope.deleteSubcon = function(index) {
+    $scope.event.subcontractors.splice($scope.event.subcontractors.indexOf($scope.event.subcons[index]._id), 1);
+    $scope.event.subcons.splice(index, 1);
+  };
 
-      $scope.deleteSubcon = function(index) {
-	$scope.event.subcontractors.splice($scope.event.subcontractors.indexOf($scope.event.subcons[index]._id), 1);
-	$scope.event.subcons.splice(index, 1);
-      };
-
-      $scope.submit = function() {
-	console.log($scope.event);
-	Events.update($scope.event)
-	  .then(function(response) {
-	    $modalInstance.close();
-	    $state.reload();
-	  }, function(err) {
-	    console.log(err);
-	});
-      };
+  $scope.submit = function() {
+    console.log($scope.event);
+    Events.update($scope.event)
+    .then(function(response) {
+      $modalInstance.close();
+      $state.reload();
+    }, function(err) {
+      console.log(err);
+    });
+  };
 }]);
