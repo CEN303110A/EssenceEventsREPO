84cc39be80d5c0abb7c22cc4305e6b63b478febd
Or Oz <oroz77@gmail.com>
2016-03-19 15:44:38 -0400

    added rentals view with photo slider. Still not aesthetic

--- client/app/rentals/rentals.controller.js
+++ client/app/rentals/rentals.controller.js
@@ -2,5 +2,35 @@
 
 angular.module('essenceEventsRepoApp')
   .controller('RentalsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    var slideItems = [
+            {
+                image: "assets/images/rentals/dresses.jpg",
+                text: "Dresses"
+            },
+            {
+                image: "assets/images/rentals/chairs.jpg",
+                text: "Chairs"
+            },
+            {
+                image: "assets/images/rentals/cloths.jpg",
+                text: "Cloths"
+            },
+            {
+                image: "assets/images/rentals/decorations.jpg",
+                text: "Decorations"
+            }
+    ];
+
+    var texts = []
+    $scope.addSlide = function(i) {
+      slides.push({
+        image: slideItems[i].image,
+        text: slideItems[i].text
+      });
+    };
+    for (var i=0; i<slideItems.length; i++) {
+      $scope.addSlide(i);
+    }
   });
