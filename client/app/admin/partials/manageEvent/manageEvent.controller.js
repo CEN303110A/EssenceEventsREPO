91859e6f8ac2002a48411ecf2810e507a6f2b2bd
nicolasiscoding <nicolascoding@gmail.com>
2016-04-02 13:21:19 -0400

    added delete functionality

--- client/app/admin/partials/manageEvent/manageEvent.controller.js
+++ client/app/admin/partials/manageEvent/manageEvent.controller.js
@@ -1,46 +1,46 @@
 'use strict';
 
 angular.module('essenceEventsRepoApp.admin')
-  .controller('ManageEventCtrl', ['$scope', '$state', '$modal', '$q', 'Events', 'Auth', function ($scope, $state, $modal, $q, Events, Auth) {
-    $scope.filterPast = function() {
-      return function(item) {
-	var date = new Date();
-	return (new Date(item.date) < date.setHours(date.getHours()-5));
-      };
+.controller('ManageEventCtrl', ['$scope', '$state', '$modal', '$q', 'Events', 'Auth', function ($scope, $state, $modal, $q, Events, Auth) {
+  $scope.filterPast = function() {
+    return function(item) {
+      var date = new Date();
+      return (new Date(item.date) < date.setHours(date.getHours()-5));
     };
-    $scope.filterCurrent = function() {
-      return function(item) {
-	var date = new Date();
-	return (new Date(item.date) > date.setHours(date.getHours()-5));
-      };
+  };
+  $scope.filterCurrent = function() {
+    return function(item) {
+      var date = new Date();
+      return (new Date(item.date) > date.setHours(date.getHours()-5));
     };
-    $scope.getEvents = function() {
-      Events.getAll()
-	.then(function(response) {
-	  $scope.events = response.data;
-	  $scope.events.forEach(function(event) {
-	    Auth.getById(event.userId)
-	      .then(function(response) {
-		event.userName = response.data.name;
-	      }, function(err) {
-		//do something
-	    });
-	  });
-	}, function(error) {
-	  //do something
+  };
+  $scope.getEvents = function() {
+    Events.getAll()
+    .then(function(response) {
+      $scope.events = response.data;
+      $scope.events.forEach(function(event) {
+        Auth.getById(event.userId)
+        .then(function(response) {
+          event.userName = response.data.name;
+        }, function(err) {
+          //do something
+        });
       });
-    };
-    $scope.openModal = function(event) {
-      var modalInstance = $modal.open({
-        animation: true,
-        templateUrl: 'app/admin/partials/manageEvent/manageEventModal/manageEventModal.html',
-        controller: 'ManageEventModalCtrl',
-        resolve: {
-          event: function()
-          {
-            return event;
-          }
+    }, function(error) {
+      //do something
+    });
+  };
+  $scope.openModal = function(event) {
+    var modalInstance = $modal.open({
+      animation: true,
+      templateUrl: 'app/admin/partials/manageEvent/manageEventModal/manageEventModal.html',
+      controller: 'ManageEventModalCtrl',
+      resolve: {
+        event: function()
+        {
+          return event;
         }
-      });
-    };
+      }
+    });
+  };
 }]);
