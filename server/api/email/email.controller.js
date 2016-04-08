e717c8769ed5f116f325e7a443f54d473134cd50
Taariq Imami <timami@ufl.edu>
2016-04-08 13:27:15 -0400

    eevert "Merge branch 'development' of https://github.com/CEN303110A/EssenceEventsREPO into development"
    This reverts commit 73157c58c32f6628857e68648333baa8e4579f3f, reversing
    changes made to 0a1536e65e454904261f8da26e9081df4664f647.

--- server/api/email/email.controller.js
+++ server/api/email/email.controller.js
@@ -3,6 +3,7 @@
 import config from '../../config/environment';
 var nodemailer = require('nodemailer');
 
+
 /*
   For this email to work, please use a gmail account and put it in the developent.js
   It should follow this format:
@@ -18,7 +19,7 @@ var nodemailer = require('nodemailer');
 export function email(req, res)
 {
   var email = req.body;
-  var user = config.essEventsEmail.user;
+  var user = config.essEventsEmail.user;  //truncated email
   var pass = config.essEventsEmail.password;
   var transporter = nodemailer.createTransport('smtps://' + user + '%40gmail.com:' + pass + '@smtp.gmail.com');
   var mailOptions = {
