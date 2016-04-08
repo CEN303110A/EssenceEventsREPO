144fe508681b24cdb1485910b38693cf75ad45b9
WilliamWickerson <willie7296@gmail.com>
2016-04-08 01:06:58 -0400

    small nodemailer change

--- server/api/email/email.controller.js
+++ server/api/email/email.controller.js
@@ -4,5 +4,9 @@ import config from '../../config/environment';
 
 export function email(req, res) {
   var email = req.body;
+  var user;
+  var pass;
+  var transporter = nodemailer.createTransport('smtps://' + user + '%40gmail.com:' + pass + '@smtp.gmail.com');
+    //fill in user and pass
   //use nodemailer here and call res.send('message here') if it succeeds and res.status(400).end() if it fails
 }
