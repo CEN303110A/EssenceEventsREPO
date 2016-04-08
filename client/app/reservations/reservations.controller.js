17de99ecee44cbb5084784b06660055b4def59f5
nicolasiscoding <nicolascoding@gmail.com>
2016-04-08 13:57:04 -0400

    readded Chase's work for nodemailer

--- client/app/reservations/reservations.controller.js
+++ client/app/reservations/reservations.controller.js
@@ -1,37 +1,49 @@
 'use strict';
 
 angular.module('essenceEventsRepoApp')
-  .controller('ReservationsCtrl', ['$scope', 'Auth', '$http', '$location', function ($scope, Auth, $http, $location) {
-    var isDraggable = !('ontouchstart' in document.documentElement);
+.controller('ReservationsCtrl', ['$scope', 'Auth', '$http', '$location', function ($scope, Auth, $http, $location) {
+  var isDraggable = !('ontouchstart' in document.documentElement);
 
-    $scope.map = { center: { latitude: 29.65253, longitude: -82.330276 }, zoom: 17, options: {draggable: isDraggable}};
-    $scope.coordsUpdates = 0;
-    $scope.dynamicMoveCtr = 0;
-    $scope.marker = {
-      id:0,
-      coords: {
-        latitude: 29.65253,
-        longitude: -82.330276
-      },
-      options: {draggable: false}
+  $scope.emailSuccess = false;		 
+  $scope.emailError = false;
+
+  $scope.map = { center: { latitude: 29.65253, longitude: -82.330276 }, zoom: 17, options: {draggable: isDraggable}};
+  $scope.coordsUpdates = 0;
+  $scope.dynamicMoveCtr = 0;
+  $scope.marker = {
+    id:0,
+    coords: {
+      latitude: 29.65253,
+      longitude: -82.330276
+    },
+    options: {draggable: false}
+  };
+
+  $scope.submit = function() {
+    var email = {
+      firstName: $scope.fname,
+      lastName: $scope.lname,
+      address: $scope.email,
+      phone: $scope.phone,
+      message: $scope.message
     };
 
-    $scope.submit = function() {
-      var email = {
-        firstName: $scope.fname,
-        lastName: $scope.lname,
-        address: $scope.email,
-        phone: $scope.phone,
-        message: $scope.message
-      };
+    $http.post('http://' + $location.host() + ':' + $location.port() +'/api/email', email)
+    .then(function(response) {
+      console.log(response);
+      if (response.status === 200) {
+        $scope.fname = '';
+        $scope.lname = '';
+        $scope.email = '';
+        $scope.phone = '';
+        $scope.message = '';
+        $scope.emailSuccess = true;
+      }
+    }, function(error) {
+      console.log(error);
+      //do something else on error
 
-      $http.post('http://' + $location.host() + ':' + $location.port() +'/api/email', email)
-        .then(function(response) {
-          console.log(response);
-	        //do something on success
-	      }, function(error) {
-          console.log(error);
-	         //do something else on error
-      });
-    }
-  }]);
+      $scope.emailError = true;
+    });
+  }
+}]);
