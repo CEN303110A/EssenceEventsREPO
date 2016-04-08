abfc827cc016c1ab4214dd43e193d9cb0421ae59
Chase Farmer <chasefarmer2808@ufl.edu>
2016-04-08 12:25:56 -0400

    error and succ messages

--- server/api/email/email.controller.js
+++ server/api/email/email.controller.js
@@ -4,14 +4,10 @@ import config from '../../config/environment';
 var nodemailer = require('nodemailer');
 
 export function email(req, res) {
-  console.log('here');
   var email = req.body;
-  var user = config.essEventsEmail.user;  //truncated email
+  var user = config.essEventsEmail.user;
   var pass = config.essEventsEmail.password;
-  console.log('user: ', user, ' pass: ', pass);
   var transporter = nodemailer.createTransport('smtps://' + user + '%40gmail.com:' + pass + '@smtp.gmail.com');
-    //fill in user and pass
-  //use nodemailer here and call res.send('message here') if it succeeds and res.status(400).end() if it fails
   var mailOptions = {
     from: email.address,
     to: config.essEventsEmail.address,
