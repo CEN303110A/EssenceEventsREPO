fc35d80bcb7d5573dfbfc944a184ea5c502665a1
Chase Farmer <chasefarmer2808@ufl.edu>
2016-04-11 17:39:18 -0400

    changing file location

--- test/conf.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['createnewevent.js'],
-  capabilities: {
-    browserName: 'firefox'
-  }
-}
