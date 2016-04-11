3e2b18c7ebd904d79c18fdb271c72e6d500089c9
WilliamWickerson <willie7296@gmail.com>
2016-04-11 14:39:42 -0400

    Added phone number verification to Reset Pass

---
+++ client/app/account/login/resetModal/manageContractorModal.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('essenceEventsRepoApp.admin')
+.controller('resetModalCtrl', ['$scope', '$state', '$modalInstance', 'Email', 'email', 'vm', function ($scope, $state, $modalInstance, Email, email, vm)
+{
+  $scope.user = {
+    email: '',
+    phoneNumber: '',
+  };
+  $scope.user.email = JSON.parse(JSON.stringify(email));
+
+  $scope.reset = function() {
+    Email.resetPass($scope.user)
+      .then(function(response) {
+	vm.success = 'New password sent';
+	$modalInstance.close();
+      }, function(err) {
+	vm.errors.other = 'Reset Failed!';
+	$modalInstance.close();
+    });
+  }
+
+  $scope.cancel = function() {
+    $modalInstance.close();
+  }
+}]);
